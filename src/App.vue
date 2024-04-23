0c735702de3ad57dd9629907767b779936c233fe
hoquoctri <hoquoctri10b7@gmail.com>
2024-04-23 12:15:24 +0700

    init taskly

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <div class="headingTitle">
+            Create Tasks
+        </div>
+        <div class="test">
+        <h1>Workout</h1>
+    </div>
+    </div>
+</template>
+<style lang="scss">
+
+.headingTitle{
+    font-size: 30px;
+    font-weight:bold;
+}
+.test {
+    width: 200px;
+    height: 150px;
+    background-color: white;
+    border-radius: 20px;
+    box-shadow: rgba(17, 17, 26, 0.1) 0px 0px 16px;
+}
+</style>
